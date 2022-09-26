0de71f2bda2fa5d75a8da7219955e1d6815d6391
awwgrima <guptaagrima200@gmail.com>
2022-09-26 15:32:27 +0530

    To generate creds

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -16,9 +16,11 @@ public class Login : MonoBehaviour
     [SerializeField] private TMP_InputField passwordInputField;
 
     public static string username1 = "Sample";
+    public int num_of_logs =0;
 
    public void onLoginClick()
    {
+    
         alertText.text = "Signing in...";
         loginButton.interactable = false;
 
@@ -60,7 +62,17 @@ public class Login : MonoBehaviour
         }
         if(request.result == UnityWebRequest.Result.Success)
         {
-            if(request.downloadHandler.text != "Invalid Credentials")
+            // if(request.downloadHandler.text == "Cannot login again")
+            // {
+            //     loginButton.interactable =false;
+            //     alertText.text = "Cannot Login again";
+            // }
+            if(request.downloadHandler.text == "Invalid move")
+            {
+                 alertText.text= "Already logged in";
+                loginButton.interactable = false;
+            }
+           else if(request.downloadHandler.text != "Invalid Credentials")
             {
                 
                 loginButton.interactable = false;
@@ -68,9 +80,14 @@ public class Login : MonoBehaviour
                 alertText.text= "Welcome "+ returnedAccount.username + ((returnedAccount.adminFlag == 1) ? " Admin": "");
                 
                 username1=returnedAccount.username;
-                SceneManager.LoadScene(1);
+                //SceneManager.LoadScene(1);
 
                 }
+            // else if(request.downloadHandler.text == "Invalid move")
+            // {
+            //      alertText.text= "Already logged in";
+            //     loginButton.interactable = false;
+            // }
             else
             {
                 alertText.text= "Invalid Credentials";
